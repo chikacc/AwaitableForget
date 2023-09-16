05088f09b55c5e407030a703a534d64cc90bef0e
Chika Chang <13810485+chikacc@users.noreply.github.com>
2023-09-17 01:15:00 +0800

    feat: initial commit

---
+++ AwaitableExtensions.Tests.PlayMode/AwaitableExtensionsTests.Forget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine;
+
+// ReSharper disable PartialTypeWithSinglePart
+
+namespace AwaitableExtensions.Tests {
+    sealed partial class AwaitableExtensionsTests {
+        [Test]
+        public void Forget() {
+            var list = new List<Exception>();
+            var exception = new Exception();
+            TestCase().Invoking(x => x.Forget(list.Add, false)).Should().NotThrow();
+            list.Should().ContainSingle().Which.Should().Be(exception);
+            return;
+
+            async Awaitable TestCase() {
+                await Awaitable.MainThreadAsync();
+                throw exception;
+            }
+        }
+    }
+}
---
+++ AwaitableExtensions/AwaitableExtensions.Forget.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// ReSharper disable EmptyGeneralCatchClause
+// ReSharper disable PartialTypeWithSinglePart
+
+namespace AwaitableExtensions;
+
+[PublicAPI]
+public static partial class AwaitableExtensions {
+    public static Action<Exception> ExceptionHandler { get; set; } = Debug.LogException;
+    public static bool HandleExceptionOnMainThread { get; set; } = true;
+
+#if UNITY_EDITOR
+    [InitializeOnEnterPlayMode]
+    static void OnEnterPlayModeInEditor(EnterPlayModeOptions options) {
+        ExceptionHandler = Debug.LogException;
+        HandleExceptionOnMainThread = true;
+    }
+#endif
+
+    public static void Forget(this Awaitable task) {
+        Forget(task, ExceptionHandler, HandleExceptionOnMainThread);
+    }
+
+    public static void Forget(this Awaitable task, Action<Exception> exceptionHandler,
+        bool handleExceptionOnMainThread = true) {
+        if (handleExceptionOnMainThread) ForgetCore(HandleExceptionOnMainThread(task, exceptionHandler));
+        else ForgetCore(task, exceptionHandler);
+        return;
+
+        static async Awaitable HandleExceptionOnMainThread(Awaitable task, Action<Exception> exceptionHandler) {
+            try {
+                await task;
+            } catch (Exception e) {
+                if (exceptionHandler is null) return;
+                try {
+                    await Awaitable.MainThreadAsync();
+                    exceptionHandler(e);
+                } catch { }
+            }
+        }
+    }
+
+    static void ForgetCore(Awaitable task, Action<Exception> exceptionHandler = null) {
+        var awaiter = task.GetAwaiter();
+        if (awaiter.IsCompleted) GetResult(awaiter, exceptionHandler);
+        else awaiter.OnCompleted(() => GetResult(awaiter, exceptionHandler));
+        return;
+
+        static void GetResult(Awaitable.Awaiter awaiter, Action<Exception> exceptionHandler) {
+            try {
+                awaiter.GetResult();
+            } catch (Exception e) {
+                if (exceptionHandler is null) return;
+                try {
+                    exceptionHandler(e);
+                } catch { }
+            }
+        }
+    }
+
+    public static void Forget<T>(this Awaitable<T> task) {
+        Forget(task, ExceptionHandler, HandleExceptionOnMainThread);
+    }
+
+    public static void Forget<T>(this Awaitable<T> task, Action<Exception> exceptionHandler,
+        bool handleExceptionOnMainThread = true) {
+        if (handleExceptionOnMainThread) ForgetCore(HandleExceptionOnMainThread(task, exceptionHandler));
+        else ForgetCore(task, exceptionHandler);
+        return;
+
+        static async Awaitable HandleExceptionOnMainThread(Awaitable<T> task, Action<Exception> exceptionHandler) {
+            try {
+                await task;
+            } catch (Exception e) {
+                if (exceptionHandler is null) return;
+                try {
+                    await Awaitable.MainThreadAsync();
+                    exceptionHandler(e);
+                } catch { }
+            }
+        }
+    }
+
+    static void ForgetCore<T>(Awaitable<T> task, Action<Exception> exceptionHandler = null) {
+        var awaiter = task.GetAwaiter();
+        if (awaiter.IsCompleted) GetResult(awaiter, exceptionHandler);
+        else awaiter.OnCompleted(() => GetResult(awaiter, exceptionHandler));
+        return;
+
+        static void GetResult(Awaitable<T>.Awaiter awaiter, Action<Exception> exceptionHandler) {
+            try {
+                awaiter.GetResult();
+            } catch (Exception e) {
+                if (exceptionHandler is null) return;
+                try {
+                    exceptionHandler(e);
+                } catch { }
+            }
+        }
+    }
+}
